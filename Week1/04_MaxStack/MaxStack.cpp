fb41967619f1a2d38442b81cc855578cd5799bf4
eXetrum <shishimorovmaksim@gmail.com>
2022-09-08 20:25:46 +0300

    MaxStack complete

--- Week1/04_MaxStack/MaxStack.cpp
+++ Week1/04_MaxStack/MaxStack.cpp
@@ -3,22 +3,31 @@
 #include <stack>
 using namespace std;
 
-template <class Type>
+template <class Type=int>
 class MaxStack {
 	stack<Type> values;
-	stack<Type> max;
+	stack<Type> cur_max;
 public:
 	void push(const Type& item) {
 		values.push(item);
-		Type max_item = item;
-		if (max.empty() || item > max.top())
-			max.push(item);
+		if (cur_max.empty() || item > cur_max.top())
+			cur_max.push(item);
+		else
+			cur_max.push(cur_max.top());
+	}
+	
+	Type max() const { return cur_max.top(); }
+	
+	void pop() {
+		values.pop();
+		cur_max.pop();
 	}
 };
 
 int main() {
 	string cmd;
 	int n;
+	MaxStack<> st;
 	
 	cin >> n;
 	for (int i = 0; i < n; ++i) {
@@ -26,8 +35,11 @@ int main() {
 		if (cmd == "push") {
 			int value;
 			cin >> value;
+			st.push(value);
 		} else if(cmd == "max") {
+			cout << st.max() << endl;
 		} else if(cmd == "pop") {
+			st.pop();
 		}
 	}
 	return 0;
