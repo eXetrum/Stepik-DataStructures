7dae539d5f06a4872e547e83ed2458939712c579
eXetrum <shishimorovmaksim@gmail.com>
2022-09-10 20:55:07 +0300

    Create map.hpp

---
+++ include/map.hpp
@@ -0,0 +1,17 @@
+#ifndef __MAP__HPP__
+#define __MAP__HPP__
+
+#include <stdexcept>
+
+template <class Type>
+class map {
+private:
+protected:
+
+public:
+	map() {}
+	map(const map& other) {}
+	~map() { }
+};
+
+#endif // !__MAP__HPP__
