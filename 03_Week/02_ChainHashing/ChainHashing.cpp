2f87413a34ecc5adc79ffedf54d386a4760ae8cb
eXetrum <shishimorovmaksim@gmail.com>
2022-09-11 03:13:53 +0300

    task 2, code skeleton

--- 03_Week/02_ChainHashing/ChainHashing.cpp
+++ 03_Week/02_ChainHashing/ChainHashing.cpp
@@ -1,7 +1,30 @@
 #include <iostream>
+#include <string>
 #include <vector>
+#include <list>
 #include <map>
 using namespace std;
+
+template <class key_t=string>
+class hash_table {
+    vector<list<key_t>> bucket;
+
+    void add(const key_t& key) {
+        // TODO
+    }
+
+    bool find(const key_t& key) const {
+        // TODO
+    }
+
+    void check(size_t i) const {
+        // TODO
+    }
+
+    void remove(const key_t& key) {
+        // TODO
+    }
+};
 
 int main() {
 
