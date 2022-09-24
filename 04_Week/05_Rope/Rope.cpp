dda98f2d26142f3ee2f52cb22bf0c4512420e22f
eXetrum <shishimorovmaksim@gmail.com>
2022-09-24 07:29:46 +0300

    Week4, Task4, Complete

--- 04_Week/05_Rope/Rope.cpp
+++ 04_Week/05_Rope/Rope.cpp
@@ -6,296 +6,17 @@
 
 using namespace std;
 
+#define DEBUG
+
+#ifdef DEBUG
 #include <vld\vld.h>
-
-
-//
-//#ifdef NDEBUG
-//#define assert(condition, message) 0
-//#else
-//#define assert(condition, message)\
-//   (!(condition)) ?\
-//      (std::cerr << "Assertion failed: (" << #condition << "), "\
-//      << "function " << __FUNCTION__\
-//      << ", file " << __FILE__\
-//      << ", line " << __LINE__ << "."\
-//      << std::endl << message << std::endl, abort(), 0) : 1
-//#endif
-//
-//template <class Type = long long>
-//class splay_tree_t {
-//private:
-//    splay_tree_t(const splay_tree_t&) = delete;
-//    splay_tree_t& operator=(const splay_tree_t&) = delete;
-//private:
-//    struct node_t {
-//        int offset;
-//        int weight;
-//        struct node_t* parent;
-//        struct node_t* left;
-//        struct node_t* right;
-//
-//        /*node_t(const Type& key)
-//            : key(key), parent(nullptr), left(nullptr), right(nullptr) { }*/
-//
-//        node_t(int offset, int weight) 
-//            : offset(offset), weight(weight), 
-//            parent(nullptr), left(nullptr), right(nullptr) { }
-//    };
-//protected:
-//    node_t* root;
-//    const char* m_data;
-//    int m_len;
-//public:
-//    splay_tree_t(const char* data, int len)
-//        : root(nullptr), m_data(data), m_len(len) { 
-//        root = new node_t(0, len);
-//    }
-//    ~splay_tree_t() { clear(); }
-//private:
-//    void destroy_tree_helper(node_t* node) {
-//        if (node == nullptr) return;
-//        destroy_tree_helper(node->left);
-//        destroy_tree_helper(node->right);
-//        delete node;
-//    }
-//
-//    void in_order_traverse_helper(node_t* node, function<void(const char* data, int offset, int weight)> f) {
-//        if (node == nullptr) return;
-//        in_order_traverse_helper(node->left, f);
-//        f(m_data, node->offset, node->weight);
-//        in_order_traverse_helper(node->right, f);
-//    }
-//
-//    inline void small_rotation(node_t* v) {
-//        node_t* parent = v->parent;
-//        if (parent == nullptr) { return; }
-//
-//        node_t* grandparent = v->parent->parent;
-//        if (parent->left == v) {
-//            node_t* m = v->right;
-//            v->right = parent;
-//            parent->left = m;
-//        } else {
-//            node_t* m = v->left;
-//            v->left = parent;
-//            parent->right = m;
-//        }
-//        update(parent);
-//        update(v);
-//        v->parent = grandparent;
-//        if (grandparent != nullptr) {
-//            if (grandparent->left == parent) {
-//                grandparent->left = v;
-//            } else {
-//                grandparent->right = v;
-//            }
-//        }
-//    }
-//
-//    inline void big_rotation(node_t* v) {
-//        if ((v->parent->left == v && v->parent->parent->left == v->parent)
-//            || (v->parent->right == v && v->parent->parent->right == v->parent)) {
-//            // Zig-zig
-//            small_rotation(v->parent);
-//            small_rotation(v);
-//        }
-//        else {
-//            // Zig-zag
-//            small_rotation(v);
-//            small_rotation(v);
-//        }
-//    }
-//
-//    inline void update(node_t* v) {
-//        if (v == nullptr) return;
-//        //v->sum = v->key
-//            //+ (v->left != nullptr ? v->left->sum : 0)
-//            //+ (v->right != nullptr ? v->right->sum : 0);
-//        v->weight = 
-//            + (v->left != nullptr ? v->left->weight : 0)
-//            + (v->right != nullptr ? v->right->weight : 0);
-//        if (v->left != nullptr) { v->left->parent = v; }
-//        if (v->right != nullptr) { v->right->parent = v; }
-//    }
-//
-//
-//    inline node_t* splay(node_t* u) {
-//        if (u == nullptr) return nullptr;
-//        while (u->parent != nullptr) {
-//            // No grand parent (we r close to root), do single rotation and stop
-//            if (u->parent->parent == nullptr) {
-//                small_rotation(u);
-//                break;
-//            }
-//            // Keep pushing 'u' to the root
-//            big_rotation(u);
-//        }
-//        return u;
-//    }
-//
-//    inline node_t* merge(node_t* left, node_t* right) {
-//
-//        // No left subtree -> new root is right subtree
-//        if (left == nullptr) return right;
-//
-//        // No right subtree -> new root is left subtree
-//        if (right == nullptr) return left;
-//
-//        // Min in right subtree
-//        while (right->left != nullptr) { right = right->left; }
-//
-//        // Move to the root and make assign left subtree as left child
-//        right = splay(right);
-//        right->left = left;
-//
-//        // Update sum values 
-//        update(right);
-//
-//        return right;
-//    }
-//
-//    inline pair<node_t*, node_t*> split(node_t* root, int idx) {
-//        pair<node_t*, node_t*> result;
-//
-//        pair<node_t*, node_t*> find_and_root = find(root, idx);
-//        root = find_and_root.second;
-//        result.second = find_and_root.first;
-//        if (result.second == nullptr) {
-//            result.first = root;
-//            return result;
-//        }
-//        result.second = splay(result.second);
-//        result.first = result.second->left;
-//        result.second->left = nullptr;
-//
-//        // No more parent for left subtree
-//        if (result.first != nullptr) {
-//            result.first->parent = nullptr;
-//        }
-//
-//        // Update sum value for left/right subtree
-//        update(result.first);
-//        update(result.second);
-//
-//        return result;
-//    }
-//
-//    inline pair<node_t*, node_t*> find(node_t* root, int idx) {
-//        node_t* v = root;
-//        node_t* last = root;
-//        node_t* next = nullptr;
-//        if (root == nullptr) return make_pair(nullptr, nullptr);
-//
-//        while (v->left != nullptr && idx > 0) {
-//            last = v;
-//            if (v->left->weight >= idx) {
-//                v = v->left;
-//                next = v;
-//            } else {
-//                next = v;
-//                idx -= v->left->weight;
-//                v = v->right;                
-//            }
-//            /*if (v->key >= key
-//                && (next == nullptr || v->key < next->key)) {
-//                next = v;
-//            }
-//            last = v;
-//            if (v->key == key) { break; }
-//            if (v->key < key) { v = v->right; }
-//            else { v = v->left; }*/
-//        }
-//        root = splay(last);
-//        return make_pair(next, root);
-//    }
-//
-//    inline char& get(node_t* node, int idx) {
-//        while(node->left) {
-//            if (node->left->weight >= idx) {
-//                node = node->left;
-//            } else {
-//                idx -= node->left->weight;
-//                node = node->right;
-//            }
-//        }
-//
-//        char* ptr = const_cast<char*>(m_data);
-//        return ptr[node->offset + idx];
-//    }
-//public:
-//    void in_order_traverse(function<void(const char*, int, int)> func) { in_order_traverse_helper(root, func); }
-//
-//    void clear() { destroy_tree_helper(root); }
-//    bool empty() const { return root == nullptr; }
-//
-//    void insert(const Type& x) {
-//        node_t* left = nullptr;
-//        node_t* right = nullptr;
-//        node_t* new_vertex = nullptr;
-//        pair<node_t*, node_t*> leftRight = split(root, x);
-//        left = leftRight.first;
-//        right = leftRight.second;
-//        if (right == nullptr || right->key != x) { new_vertex = new node_t(x); }
-//        root = merge(merge(left, new_vertex), right);
-//    }
-//
-//    void remove(const Type& x) {
-//        // Use split and merge technique
-//        pair<node_t*, node_t*> leftMiddle = split(root, x);
-//        node_t* left = leftMiddle.first;
-//        node_t* middle = leftMiddle.second;
-//        pair<node_t*, node_t*> middleRight = split(middle, x + 1);
-//        middle = middleRight.first;
-//        node_t* right = middleRight.second;
-//        if (middle == nullptr || middle->key != x) {
-//            root = merge(merge(left, middle), right);
-//        } else {
-//            delete middle;
-//            root = merge(left, right);
-//        }
-//    }
-//
-//    char& operator[](int idx) {
-//        return get(root, idx);
-//    }
-//
-//    bool find(int idx) {
-//        node_t* left = nullptr;
-//        node_t* right = nullptr;
-//        pair<node_t*, node_t*> leftRight = split(root, idx);
-//        left = leftRight.first;
-//        right = leftRight.second;
-//        root = merge(left, right);
-//
-//        return right != nullptr;//&& right->key == x;
-//    }
-//
-//    Type range_sum(const Type& L, const Type& R) {
-//        pair<node_t*, node_t*> leftMiddle = split(root, L);
-//        node_t* left = leftMiddle.first;
-//        node_t* middle = leftMiddle.second;
-//        pair<node_t*, node_t*> middleRight = split(middle, R + 1);
-//        middle = middleRight.first;
-//        node_t* right = middleRight.second;
-//        Type ans = 0;
-//        // Complete the implementation of sum
-//        if (middle != nullptr)
-//            ans = middle->sum;
-//        middle = merge(middle, right);
-//        root = merge(left, middle);
-//
-//        return ans;
-//    }
-//};
-//
-
+#endif
 
 template <class T>
-class Rope {
+class rope_array_t {
 private:
 	class node_t {
-		friend Rope;
+		friend rope_array_t;
 
 		static int get_weight(node_t* left, node_t* right) {
 			return (left != nullptr ? left->weight : 0)
@@ -303,25 +24,15 @@ private:
 		}
 
 	protected:
-		int offset;
-		int slice_size;
+		T data;
 		int weight;
 		node_t* left;
 		node_t* right;
 		node_t* parent;
 	public:
-		inline void reset(node_t* left, node_t* right) {
-			this->left = left;
-			this->right = right;
-			weight = get_weight(left, right);
-		}
-
-		explicit node_t(int offset, int weight)
-			: offset(offset), slice_size(weight), weight(weight),
+		explicit node_t(const T& data)
+			: data(data), weight(1),
 			left(nullptr), right(nullptr), parent(nullptr) { }
-
-		explicit node_t(node_t* left, node_t* right) 
-			: offset(-1), slice_size(0), weight(0), parent(nullptr) { reset(left, right); }
 	};
 
 	inline void small_rotation(node_t* v) {
@@ -333,8 +44,7 @@ private:
 			node_t* m = v->right;
 			v->right = parent;
 			parent->left = m;
-		}
-		else {
+		} else {
 			node_t* m = v->left;
 			v->left = parent;
 			parent->right = m;
@@ -366,7 +76,7 @@ private:
 
 	inline void update(node_t* v) {
 		if (v == nullptr) return;
-		v->weight = v->slice_size//v->key
+		v->weight = 1
 			+ (v->left != nullptr ? v->left->weight : 0)
 			+ (v->right != nullptr ? v->right->weight : 0);
 		if (v->left != nullptr) { v->left->parent = v; }
@@ -398,10 +108,10 @@ private:
 		delete node;
 	}
 
-	inline void in_order_traverse_helper(node_t* node, function<void(const T*, int, int)> f) {
+	inline void in_order_traverse_helper(node_t* node, function<void(const T&)> f) {
 		if (node == nullptr) return;
 		in_order_traverse_helper(node->left, f);
-		f(m_data, node->offset, node->weight);
+		f(node->data);
 		in_order_traverse_helper(node->right, f);
 	}
 
@@ -423,128 +133,84 @@ private:
         update(right);
 
         return right;
-
-		//return new node_t(L, R);
 	}
 
-	
-
 	pair<node_t*, node_t*> split(node_t* node, int i) {
+		// Find node by index, splay, easy split now
 		node_t* tree1 = nullptr, * tree2 = nullptr;
 		if (node == nullptr) return make_pair(nullptr, nullptr);
-		if (node->left) {
-			if (node->left->weight >= i) {
-				auto res = split(node->left, i);
-				tree1 = res.first;
-				//node->reset(res.second, node->right);
-				//tree2 = node;
+		node_t* mid_node = get_node(node, i);
+#ifdef DEBUG
+		cout << "split: [idx=" << i << "], node=" << (mid_node != nullptr ? mid_node->data : '0') << endl;
+#endif
+		mid_node = splay(mid_node);
+		if(mid_node == nullptr) return make_pair(node, nullptr);
+		tree1 = mid_node->left;
+		if(tree1 != nullptr)
+			tree1->parent = nullptr;
 
-				//tree2 = new node_t(res.second, node->right);
-				tree2 = merge(res.second, node->right);
-
-				//tree2 = node->reset(res.second, node->right);
-			} else {
-				auto res = split(node->right, i - node->left->weight);
-				//tree1 = new node_t(node->left, res.first);
-				tree1 = merge(node->left, res.first);
-				////node->reset(node->left, res.first);
-				////tree1 = node;
-				//tree1 = node->reset(node->left, res.first);
-				tree2 = res.second;
-			}
-		} else {
-			//substr(node->data, 0, i);
-			size_t w = node->weight;
-			tree1 = new node_t(node->offset, i);			
-			if ((int)w - i >= 0) {
-				tree2 = new node_t(node->offset + i, w - i);
-			}
-			
+		tree2 = mid_node;
+		if (tree2 != nullptr) {
+			tree2->left = nullptr;
+			tree2->parent = nullptr;
 		}
-		delete node;
-
+		update(tree1);
+		update(tree2);
 
 		return make_pair(tree1, tree2);
 	}
 
+	// Explicit keys 
+	node_t* get_node(node_t* node, int i) {
+		int idx = 0;
+		while (node != nullptr) {
+			if (node->left != nullptr) {
+				if (node->left->weight > i) {
+					node = node->left;
+				} else if(node->left->weight == i) {
+					return node;
+				} else {
+					i = i - node->left->weight - 1;
+					node = node->right;
+				}
+			} else if(node->right != nullptr) {
+				if (node->weight - 1 == i + node->right->weight)
+					return node;
 
-	/*node_t* merge(node_t* L, node_t* R) {
-		if (L == nullptr) return R;
-		if (R == nullptr) return L;
-
-
-		return new node_t(L, R);
-	}*/
-
-	//node_t* insert(node_t* node, int idx, node_t* insertant) {
-	//	if (idx == 0)
-	//		return merge(insertant, node);
-	//	if (idx >= node->w)
-	//		return merge(node, insertant);
-
-	//	auto res = split(node, idx);
-	//	auto prefix = res.first;
-	//	auto postfix = res.second;
-
-	//	return merge(merge(prefix, insertant), postfix);
-	//	//auto it = split(node, insertIndex);
-	//	//node_t* tree1 = it.first, * tree3 = it.second;
-	//	//node_t* tree2 = new node_t(data, len);
-	//	//return merge(merge(tree1, tree2), tree3);
-	//}
+				node = node->right;
+				i = i - 1;
+			} else if(i == 0) {
+				return node;
+			} else {
+				return nullptr;
+			}
+		}
+		return nullptr;
+	}
 
 	T& get(node_t* node, int i) { 
-		if (node->left != nullptr) {
-			if (node->left->weight >= i) { return get(node->left, i); }
-			return get(node->right, i - node->left->weight);
-		}
-		T* ptr = const_cast<T*>(m_data);
-		return ptr[node->offset + i];
+		node = get_node(node, i);
+		if (node == nullptr) throw std::out_of_range("Index out of bounds");
+		return node->data;
 	}
-
-
-	/*node_t* insert(node_t* node, int idx, const T* data, int len) {
-		node_t* left = nullptr;
-		node_t* right = nullptr;
-		node_t* new_vertex = nullptr;
-		pair<node_t*, node_t*> leftRight = split(root, idx);
-		left = leftRight.first;
-		right = leftRight.second;
-		if (right == nullptr || right->key != x) { new_vertex = new node_t(x); }
-		return merge(merge(left, new_vertex), right);
-	}*/
 public:
-	Rope(const T* data, int len)
+	rope_array_t(const T* data, int len)
 		: root(nullptr), m_data(data), m_size(len) {
 		
-		root = new node_t(0, len);
+		for (int i = 0; i < len; ++i) {
+			root = splay(merge(root, new node_t(data[i])));
+		}
 	}
 
-	~Rope() { clear(); }
+	~rope_array_t() { clear(); }
 
 	void clear() { clear_helper(root); root = nullptr; }
-	void in_order_traverse(function<void(const T*, int, int)> func) { in_order_traverse_helper(root, func); }
+	void in_order_traverse(function<void(const T&)> func) { in_order_traverse_helper(root, func); }
 	T& operator[](size_t i) { return get(root, i); }
-
-	/*void insert(int idx, const T* data, int len) {
-		root = insert(root, idx, data, len);
-	}*/
-
-	/*void cut_and_insert2(int i, int j, int k) {
-		if (i == 0) {
-			auto it = split(root, j - i + 1);
-			root = insert(it.first, k, it.second);
-			return;
-		}
-
-		auto L = split(root, i);
-		auto R = split(L.second, j - i + 1);
-		root = insert(merge(L.first, R.second), k, R.first);
-	}*/
 
 	void cut_and_insert(int i, int j, int k) {
 		if (i == 0) {
-			if(k == 0) { return; } // substr from start and append to start
+			if(k == 0) { return; } // substr from 0 index and prepend result
 
 			auto R = split(root, j + 1);
 			// R.first -> substr
@@ -557,11 +223,34 @@ public:
 			auto L = split(root, i);
 			auto prefix = L.first;
 
+#ifdef DEBUG
+			std::function<void(const T&)> f = [](const T& data) { cout << data << "->"; };
+			cout << endl << "prefix: " << endl;
+			root = prefix;
+			in_order_traverse(f);
+#endif
+
 			auto R = split(L.second, j - i + 1);
 			auto substr = R.first;
 			auto postfix = R.second;
 
+#ifdef DEBUG
+			cout << endl << "substr: " << endl;
+			root = substr;
+			in_order_traverse(f);
+
+			cout << endl << "postfix: " << endl;
+			root = postfix;
+			in_order_traverse(f);
+#endif
+
 			auto remaining = merge(prefix, postfix);
+
+#ifdef DEBUG
+			cout << endl << "remainder (prefix+postfix): " << endl;
+			root = remaining;
+			in_order_traverse(f);
+#endif
 
 			if (k == 0) {
 				// L.1]L.2....[
@@ -569,76 +258,63 @@ public:
 			} else {
 				auto M = split(remaining, k);
 
+#ifdef DEBUG
+				cout << endl << "M.L split by k: " << endl;
+				root = M.first;
+				in_order_traverse(f);
+
+				cout << endl << "M.R split by k: " << endl;
+				root = M.second;
+				in_order_traverse(f);
+#endif
+
 				// Substr + right part of the remaining str
 				auto substr_and_R = merge(substr, M.second);
+
+#ifdef DEBUG
+				cout << endl << "substr + M.R: " << endl;
+				root = substr_and_R;
+				in_order_traverse(f);
+#endif
+
 				root = merge(M.first, substr_and_R);
-				//root = merge(M.first, merge(R.first, M.second));
 			}
 		}
 	}
 };
 
 
-
-
-///////////////////////////////////////////////////////////////////////////////////
-
-
-
 int main() {
 	string S;
 	int q, i, j, k;
-	function<void(const char*, int, int)> f = [](const char* data, int offset, int len) {
-		if (data == nullptr || offset == -1) return;
-		const char* ptr = &data[offset];
-		for (size_t i = 0; i < len; ++i)
-			cout << ptr[i];
-		//cout << " => ";
+	function<void(const char&)> f = [](const char& data) {
+#ifdef DEBUG
+		cout << data << " ";
+#else 
+		cout << data;
+#endif
 	};
 
 	cin >> S >> q;
 
-	Rope<char> tree(S.c_str(), S.size());
-    //splay_tree_t<char> tree(S.c_str(), S.size());
-    //SplayTree<char> tree(S.begin(), S.end());
-
+	rope_array_t<char> tree(S.c_str(), S.size());
+#ifdef DEBUG
 	tree.in_order_traverse(f);
     cout << endl;
     for (int i = 0; i < S.size(); ++i) {
-        cout << tree[i] << " " << endl;
+        cout << i << " " << tree[i] << " " << endl;
     }
 	//cout <<  << endl;
     tree.in_order_traverse(f); cout << endl;
+#endif
 	while (q-- > 0) {
 		cin >> i >> j >> k;
 		tree.cut_and_insert(i, j, k);
+#ifdef DEBUG
 		tree.in_order_traverse(f); cout << endl;
+#endif
 	}
 	tree.in_order_traverse(f); cout << endl;
-	//while (q-- > 0) {
-	//	cin >> i >> j >> k;
- //       SplayTree<char> substr;
- //       if (j + 1 < tree.size()) {
- //           auto postfix = tree.split(j + 1);
- //           substr = tree.split(i);
- //           tree.merge(std::move(postfix));
- //       } else {
- //           substr = tree.split(i);
- //       }
- //       /*K.merge(std::move(substr));
- //       K.merge(std::move(tree));
- //       tree = std::move(K);*/
- //       if (k == 0) {
- //           substr.merge(std::move(tree));
- //           tree = std::move(substr);
- //       } else {
- //           auto K = tree.split(k);
- //           //K.merge(std::move(substr));
- //           substr.merge(std::move(K));
- //           tree.merge(std::move(substr));
- //       }
-	//}
- //   for (auto i : tree) { cout << i; }
 
 	return 0;
 }
